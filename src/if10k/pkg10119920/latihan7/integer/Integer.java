e85507f7a7f9098c09e2f6ca2194d07d8eeda53b
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-18 17:14:48 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan7/integer/Integer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan7.integer;
+
+
+/**
+ *
+ * @author
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class Integer{
+    
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int: " + i);
+        System.out.println("long : " + l);
+        
+    }
+
+
+}
